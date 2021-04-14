5e184465792c4061462b6aeebcac999f61b3f616
Sjoerd Vellinga <sjoerd.vellinga@gmail.com>
2021-04-14 16:28:09 +0200

    Activate front-end

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -6,9 +6,9 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: '', // the auth0 domain prefix
-    audience: '', // the audience set for the auth0 app
-    clientId: '', // the client id generated for the auth0 app
+    url: 'rellow.eu', // the auth0 domain prefix
+    audience: 'CoffeeShop', // the audience set for the auth0 app
+    clientId: 'smji1ZfvD8bdBmf9ijcpDt00hxGQxLa9', // the client id generated for the auth0 app
     callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
   }
 };
